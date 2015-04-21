4c3e11dfd1c5f3a1e187118923c18b7faa459659
ghostzhang <ghostzhang@cssforest.org>
2015-04-21 14:45:07 +0800

    更新代码展示方式

---
+++ demo/关于CSS样式命名中的下划线/1.js
@@ -0,0 +1,7 @@
+function b(){
+  document.getElementById("a").className="try-b";
+  document.getElementById("b").className="try~b";
+  document.getElementById("c").className="try`b";
+  document.getElementById("d").className="try&b";
+  document.getElementById("e").className="try$b";
+ }
